eca945b797597c0728de4166260e28e1e16d6d26
Dries Hugaerts <dries.hugaerts@live.be>
2022-01-18 17:02:58 +0100

    Introduced YASGUI as temporary frontend SPARQL editor.

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -1,25 +1,29 @@
 defmodule Dispatcher do
   use Matcher
+
   define_accept_types [
     html: [ "text/html", "application/xhtml+html" ],
-    json: [ "application/json", "application/vnd.api+json" ]
+    json: [ "application/json", "application/vnd.api+json" ],
+    sparql: [ "application/sparql-results+json" ]
   ]
 
   @any %{}
   @json %{ accept: %{ json: true } }
   @html %{ accept: %{ html: true } }
+  @sparql %{ accept: %{ sparql: true } }
 
-  # In order to forward the 'themes' resource to the
-  # resource service, use the following forward rule:
-  #
-  # match "/themes/*path", @json do
-  #   Proxy.forward conn, path, "http://resource/themes/"
-  # end
-  #
-  # Run `docker-compose restart dispatcher` after updating
-  # this file.
+  #########
+  # SPARQL
+  match "/sparql", @html do
+    forward conn, [], "http://frontend:80"
+  end
+
+  match "/sparql", @sparql do
+    forward conn, [], "http://database:8890/sparql"
+  end
 
   match "/*_", %{ last_call: true } do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
+
 end
