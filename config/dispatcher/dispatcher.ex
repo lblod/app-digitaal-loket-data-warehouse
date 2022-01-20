3ca961c5d16cda78df00239061ba330315f3ee25
Dries Hugaerts <dries.hugaerts@live.be>
2022-01-20 13:54:07 +0100

    Added temporary redirect to root + renamed "frontend" to "yasgui" to prevent future confusion.

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -13,11 +13,16 @@ defmodule Dispatcher do
   @sparql %{ accept: %{ sparql: true } }
 
   match "/sparql", @html do
-    forward conn, [], "http://frontend:80"
+    forward conn, [], "http://yasgui:80"
   end
 
   match "/sparql", @sparql do
     forward conn, [], "http://database:8890/sparql"
+  end
+
+  # Temporary Redirect to "/sparql" endpoint
+  match "/*_", @html do
+    send_resp( conn, 307, "<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" content=\"0; url='/sparql'\" /></head><body><p>Please follow <a href=\"/sparql\">this link</a>.</p></body></html>" )
   end
 
   match "/*_", %{ last_call: true } do
