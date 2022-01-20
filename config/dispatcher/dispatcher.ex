480a99eb5bcffc7e3ecbc446ac47f3473a551bf6
Dries Hugaerts <dries.hugaerts@live.be>
2022-01-20 12:37:56 +0100

    CORS header added to identifier.

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -12,14 +12,16 @@ defmodule Dispatcher do
   @html %{ accept: %{ html: true } }
   @sparql %{ accept: %{ sparql: true } }
 
-  #########
-  # SPARQL
   match "/sparql", @html do
     forward conn, [], "http://frontend:80"
   end
 
   match "/sparql", @sparql do
     forward conn, [], "http://database:8890/sparql"
+  end
+
+  match "/*_path", @html do
+    send_resp( conn, 308, "<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" content=\"0; url='https://www.w3docs.com'\" /></head><body><p>Please follow <a href=\"https://www.w3docs.com\">this link</a>.</p></body></html>" )
   end
 
   match "/*_", %{ last_call: true } do
