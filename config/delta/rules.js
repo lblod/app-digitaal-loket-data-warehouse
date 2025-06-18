0279e986fb701dde259b1d32e447eb10fbe818e9
Aad Versteden <madnificent@gmail.com>
2025-06-18 14:30:41 +0200

    Add empty delta-notifier configuration file
    The delta notifier is added by default in modern stacks.  However, this
    reactivity is not necessary in this stack.  We're keeping the delta-notifier in
    here for now but we are giving it an empty configuration to limit the amount of
    work it will execute.

---
+++ config/delta/rules.js
@@ -0,0 +1,17 @@
+export default [
+  // {
+  //   match: {
+  //     subject: { }
+  //   },
+  //   callback: {
+  //     url: "http://resource/.mu/delta",
+  //     method: "POST"
+  //   },
+  //   options: {
+  //     resourceFormat: "v0.0.1",
+  //     gracePeriod: 250,
+  //     ignoreFromSelf: true,
+  //     foldEffectiveChanges: true
+  //   }
+  // },
+];
