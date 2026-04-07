678e34da79f3b9a9a0843155efd3da8744eb4ea9
Boris De Vloed <boris.de.vloed@gmail.com>
2026-04-07 10:06:16 +0200

    Add pro-active data monitoring

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,17 +1,36 @@
 export default [
-  // {
-  //   match: {
-  //     subject: { }
-  //   },
-  //   callback: {
-  //     url: "http://resource/.mu/delta",
-  //     method: "POST"
-  //   },
-  //   options: {
-  //     resourceFormat: "v0.0.1",
-  //     gracePeriod: 250,
-  //     ignoreFromSelf: true,
-  //     foldEffectiveChanges: true
-  //   }
-  // },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/ns/adms#status',
+      },
+    },
+    callback: {
+      url: 'http://data-monitoring/delta',
+      method: 'POST',
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+    },
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/ns/adms#status',
+      },
+    },
+    callback: {
+      url: 'http://scheduled-job-controller/delta',
+      method: 'POST',
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+    },
+  },
 ];
